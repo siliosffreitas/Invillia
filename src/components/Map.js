1e76752e661d66bf251cf06c765476090fff6872
siliosffreitas@gmail.com <codes123>
2019-09-23 23:49:51 -0300

    desenhando no mapa a posicao do usuário

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 import { View, Text, Image, ScrollView } from 'react-native'
 import { showLoader } from '../actions/MainActions'
 import { connect } from 'react-redux'
+import { consts } from '../util/consts'
 import Geolocation from '@react-native-community/geolocation';
 import { changeUserLastLocation } from '../actions/MainActions'
+import MapView, { Marker, Polyline } from 'react-native-maps'
 
 class Map extends Component {
     componentWillMount() {
@@ -14,13 +16,13 @@ class Map extends Component {
         this.props.showLoader(true)
 
         Geolocation.getCurrentPosition((info) => {
-                this.props.showLoader(false)
-                alert('Usuário encontrado')
-                console.log(info.coords)
-                this.props.changeUserLastLocation(info.coords)
-                
-                // isNeedCalculateBounds = true
-                // this.props.getStopsNexts(this.props.userLastLocation.latitude, this.props.userLastLocation.longitude, this.props.defaultDistSearch)    
+            this.props.showLoader(false)
+            alert('Usuário encontrado')
+            console.log(info.coords)
+            this.props.changeUserLastLocation(info.coords)
+
+            // isNeedCalculateBounds = true
+            // this.props.getStopsNexts(this.props.userLastLocation.latitude, this.props.userLastLocation.longitude, this.props.defaultDistSearch)    
 
         });
         // navigator.geolocation.getCurrentPosition(
@@ -28,7 +30,7 @@ class Map extends Component {
         //         this.props.showLoader(false)
         //         alert('Usuário encontrado')
         //         // this.props.changeUserLastLocation(position.coords)
-                
+
         //         // isNeedCalculateBounds = true
         //         // this.props.getStopsNexts(this.props.userLastLocation.latitude, this.props.userLastLocation.longitude, this.props.defaultDistSearch)
         //     },
@@ -42,24 +44,119 @@ class Map extends Component {
 
     render() {
         return (
-            <ScrollView>
-                <View
-                    style={{ flex: 1, alignItems: 'center', padding: 16 }} >
-                    <Text style={{ paddingTop: 32, fontSize: 18 }}>StarBus</Text>
-                    <Text style={{ fontSize: 14 }}>Versão 2.0.0</Text>
-
-                    <Text style={{ textAlign: 'center', paddingVertical: 10 }}>MAPA</Text>
-
-                    <Text >© 2016~2018  StarBus Inc</Text>
-                </View>
-            </ScrollView>
+            <View style={{ flex: 1 }}>
+                {this.renderMap()}
+                {this.calculateBounds()}
+            </View>
         );
+    }
+
+    calculateBounds() {
+
+        let coords = []
+        if (this.props.userLastLocation != null) {
+            coords = [...coords,
+            {
+                latitude: this.props.userLastLocation.latitude,
+                longitude: this.props.userLastLocation.longitude,
+            }]
+        }
+
+        // if (this.props.nextsStops != null) {
+        //     for (i = 0; i < this.props.nextsStops.length; i++) {
+        //         coords = [...coords,
+        //         {
+        //             latitude: Number(this.props.nextsStops[i].lat),
+        //             longitude: Number(this.props.nextsStops[i].long),
+        //         }]
+        //     }
+        // }
+
+        // if (this.props.currentLineTrackCoords !== null) {
+        //     for (i = 0; i < this.props.currentLineTrackCoords.length; i++) {
+        //         coords = [...coords, ...this.props.currentLineTrackCoords[i].coords]
+        //     }
+
+
+        // }
+
+        // if (this.props.currentLineTrack.length === this.props.currentLineTrackCoords.length) {
+        //     isNeedCalculateBounds = false
+        // }
+
+        if (coords.length > 0 && this.mapRef != undefined) {
+            // console.log(coords)
+            this.mapRef.fitToCoordinates(coords,
+                {
+                    edgePadding: {
+                        top: 75,
+                        right: 75,
+                        bottom: 75,
+                        left: 75
+                    },
+                    animated: false
+                }
+            )
+        }
+    }
+
+    renderMap() {
+        if (this.props.userLastLocation) {
+
+            return (
+                <MapView
+                    ini
+                    style={{
+                        flex: 1
+                    }}
+                    ref={(ref) => { this.mapRef = ref }}>
+                    {/* {this.renderNextsStops()} */}
+                    {this.renderMarkerUser()}
+                    {/* {this.renderPolylines()}
+                    {this.renderTrack()} */}
+                </MapView>
+            )
+        } else {
+            return (
+                <MapView
+                    style={{
+                        flex: 1
+                    }}
+                    region={{
+                        latitude: consts.lat_city,
+                        longitude: consts.lng_city,
+                        latitudeDelta: 0.015,
+                        longitudeDelta: 0.0121,
+                    }}
+                />
+            )
+        }
+    }
+
+    renderMarkerUser() {
+        if (this.props.userLastLocation) {
+            let coordinate = {
+                latitude: this.props.userLastLocation.latitude,
+                longitude: this.props.userLastLocation.longitude,
+            }
+
+            return (
+                <Marker
+                    key="me"
+                    coordinate={coordinate}
+                    description={this.props.keywords}
+                    // image={pin_user}
+                    title="Eu"
+                    pinColor='blue'
+                />
+            )
+        }
     }
 }
 
 mapStateToProps = state => (
     {
-        // userLastLocation: state.UserReducer.userLastLocation,
+        userLastLocation: state.MainReducer.userLastLocation,
         // nextsStops: state.StopReducer.nextsStops,
         // currentLineTrack: state.LineReducer.currentLineTrack,
         // currentLineTrackCoords: state.LineReducer.currentLineTrackCoords,
@@ -73,4 +170,4 @@ mapStateToProps = state => (
     }
 )
 
-export default connect(mapStateToProps, {  showLoader , changeUserLastLocation })(Map);
+export default connect(mapStateToProps, { showLoader, changeUserLastLocation })(Map);
---
+++ src/util/consts.js
@@ -0,0 +1,8 @@
+export const consts = {
+    time_out_request: 5000,
+    google_maps_key: "AIzaSyBniMZPcyvDJmwpmY0EjUeHP1JX0NktEgo",
+    default_radius_search_places: 50000,
+    lat_city: -5.082618,
+    lng_city: -42.790596,
+    coordenadas_city: "-5.082618,-42.790596",
+}
